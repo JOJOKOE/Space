281df7fc82e3f929177958fc271de4e4108cf504
Admin <admin@Admins-Mac.local>
2018-07-21 19:10:34 +0800

    Revert "space"
    This reverts commit a9bcaad6e259a93dd2cf3060b43cac1f87b919ea.

--- 火星調色/ViewController.swift
+++ 火星調色/ViewController.swift
@@ -9,37 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-        @IBOutlet weak var mars: UIImageView!
-        @IBOutlet weak var redSlider: UISlider!
-        @IBOutlet weak var blueSlider: UISlider!
-        @IBOutlet weak var greenSlider: UISlider!
-        @IBOutlet weak var alphaSlider: UISlider!
-    @IBAction func blue(_ sender: Any) {
-        mars.backgroundColor = UIColor(red: CGFloat(redSlider.value),
-                                       green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha:
-            CGFloat(alphaSlider.value))
-        
-    }
 
-    @IBAction func red(_ sender: Any) {
-        mars.backgroundColor = UIColor(red: CGFloat(redSlider.value),
-                                       green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha:
-            CGFloat(alphaSlider.value))
-        
-    }
-    @IBAction func green(_ sender: Any) {
-        mars.backgroundColor = UIColor(red: CGFloat(redSlider.value),
-                                       green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha:
-            CGFloat(alphaSlider.value))
-        
-    }
-    
-    @IBAction func alpha(_ sender: Any) {
-        mars.backgroundColor = UIColor(red: CGFloat(redSlider.value),
-                                       green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha:
-            CGFloat(alphaSlider.value))
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
